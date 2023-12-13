04043e9f10ff3de3e890013b7ef20d8ac35739b7
Kyle Greganti <kyle.greganti@yahoo.com>
2023-12-12 18:08:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Kyle Greganti!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
